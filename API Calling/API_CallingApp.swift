8d9ec00af99f7b550982b6dedf5b173ba30a6b7b
Owen Johnson <owenbjohnson10@gmail.com>
2023-02-28 09:25:31 -0600

    Initial Commit

---
+++ API Calling/API_CallingApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_CallingApp.swift
+//  API Calling
+//
+//  Created by Owen Johnson on 2/27/23.
+//
+
+import SwiftUI
+
+@main
+struct API_CallingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
