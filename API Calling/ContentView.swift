8bb31de1fdbb7ac62ce2855da3d40e2aaab89876
Owen Johnson <owenbjohnson10@gmail.com>
2023-03-15 09:38:25 -0500

    Finished Game

--- API Calling/ContentView.swift
+++ API Calling/ContentView.swift
@@ -7,20 +7,39 @@
 
 import SwiftUI
 
+import Foundation
+import SwiftUI
 struct ContentView: View {
+    @State private var image: String = ""
+    @State private var ping = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        if ping == true {
+            Link("See on the web", destination: URL(string: image)!)
+            AsyncImage(url: URL(string: image)!)
+                .frame(maxWidth: 300, maxHeight: 100)
+                .scaledToFit()
+
+                
         }
-        .padding()
+        Button {
+            Task {
+                let (data, _) = try await URLSession.shared.data(from: URL(string:"https://dog.ceo/api/breeds/image/random")!)
+                let decodedResponse = try? JSONDecoder().decode(Images.self, from: data)
+            
+                image = decodedResponse?.message ?? ""
+                ping = true
+            }
+            
+        } label: {
+            Text("Find Dog")
+        }
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+struct Images: Codable {
+    let message : String
 }
